bbbe90b8aa8d19f7b79823c1b159927e837cf738
vuduythai1192k6 <vthat989@gmail.com>
2026-09-18 17:47:46 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap so luong phan tu: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Nhap phan tu thu " + (i + 1) + ": ");
+            a[i] = int.Parse(Console.ReadLine());
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += a[i];
+        }
+
+        Console.WriteLine("Tong cac phan tu = " + sum);
+    }
+}
